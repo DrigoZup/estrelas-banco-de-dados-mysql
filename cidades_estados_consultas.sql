ed163583cfbaa8669b064d907376130cd7e916db
Drigo Zup <rodrigo.lima@zup.com.br>
2020-10-05 17:52:04 -0300

    Algumas consultas ao banco de dados

---
+++ cidades_estados_consultas.sql
@@ -0,0 +1,53 @@
+use estrelas;
+
+select nome, renda_percapita
+from cidade
+where numero_habitantes > 5000;
+
+select*
+from cidade
+where data_fundacao > '1432-03-12';
+
+select nome
+from cidade
+where cep = '38180000';
+
+select *
+from cidade, estado
+where cidade.estado = estado.sigla
+and estado.nome = 'São Paulo';
+
+select nome
+from cidade
+where capital = true 
+and renda_percapita > 30000;
+
+select cidade.nome, estado.nome
+from cidade, estado
+where cidade.estado = estado.sigla
+and sigla = 'SP';
+
+
+select regiao, estado.nome, cidade.nome, numero_habitantes
+from cidade, estado
+where cidade.estado = estado.sigla
+and renda_percapita > 20000 and capital = false;
+
+select *
+from cidade, estado
+where cidade.estado = estado.sigla
+and regiao = 'Sudeste';
+
+select cidade.nome, numero_habitantes
+from cidade, estado
+where cidade.estado = estado.sigla
+and regiao = 'Sudeste' and capital = false
+and sigla <> 'MG' and renda_percapita > 15000;
+
+
+
+
+
+
+
+
